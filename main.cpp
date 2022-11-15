a0fd47b186372e506e98843ce3e63f2b1547e11d
D. Neykov <metheorita@gmail.com>
2022-11-15 09:48:57 +0100

    Remove UI stuff from main branch, update CMake file

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 #include <src/omar.h>
 #include <src/TileContainer.h>
-#include <src/Panel.h>
 #include <src/Point.h>
 
 using namespace std;
@@ -13,51 +12,29 @@ int main()
 	omar::Terminal window(16.0f, ' ', "data/");
 	window.initialize();
 	
-	// these constructors are really stupid
-	Panel mainWindow(window.getDimensions().x, window.getDimensions().y, "main_window", Point(0, 0), ' ');
-	
-	Panel textBox(28, 3, "text_box", Point(40, 30), ' ');
-	textBox.blitTransparency = false;
-	
-	mainWindow.addChild(&textBox);
-	
-	TileContainer finalWindow(120, 120, ' ', Color(255, 255, 255), Color(0, 0, 0));
-	
 	window.showFps = true;
 
-	textBox.setChar(10, 2, '@');
+	window.setChar(10, 2, '@');
 	
-	textBox.setString(0, 0, "OpenGL ", Color(120, 255, 87));
-	textBox.setString(7, 0, "bitMap ", Color(255, 236, 110));
-	textBox.setString(14, 0, "ASCII ", Color(255, 102, 134));
-	textBox.setString(20, 0, "Renderer", Color(180, 120, 255));
-	textBox.setString(3, 2, "easy - portable - cool");
+	int x = 45;
+	int y = 30;
+	window.setString(0 + x, 0 + y, "OpenGL ", Color(120, 255, 87));
+	window.setString(7 + x, 0 + y, "bitMap ", Color(255, 236, 110));
+	window.setString(14+ x, 0 + y, "ASCII ", Color(255, 102, 134));
+	window.setString(20+ x, 0 + y, "Renderer", Color(180, 120, 255));
+	window.setString(3 + x, 2 + y, "easy - portable - cool");
 
 	for (int i = 0; i < 28; i++)
 	{
-		textBox.setTileColor(i, 0, 50, 50, 50);
+		window.setTileColor(i + x, 0 + y, 50, 50, 50);
 	}
-	
-	mainWindow.blitInto(window);
 
 	bool running = true;
 	while (running)
-	{
-		if (window.isLClickHeld())
-		{
-			mainWindow.click("", window.getMousePosTile());
-		}
-		window.setString(0, 2, to_string(window.getMousePosTile().x));
-		window.setString(0, 3, to_string(window.getMousePosTile().y));
-		window.setChar(0, 1, window.getKeyPress() > 0 ? window.getKeyPress() : ' ');
-		
+	{		
 		if(window.getKeyPress() == 27)
 			running = false;
 		
 		window.draw();
-		
-		window.setString(0, 1, "     ");
-		window.setString(0, 2, "     ");
-		window.setString(0, 3, "     ");
 	}
 }
